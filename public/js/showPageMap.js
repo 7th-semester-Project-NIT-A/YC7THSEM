3c0f5c847c3fd0e7e198cc8b2ba0cb937f9548ff
7th-semester-Project-NIT-A <116279633+7th-semester-Project-NIT-A@users.noreply.github.com>
2022-10-20 18:47:44 +0530

    Add files via upload

---
+++ public/js/showPageMap.js
@@ -0,0 +1,20 @@
+mapboxgl.accessToken = mapToken;
+const campground = JSON.parse(document.getElementById("yy").innerText);
+const map = new mapboxgl.Map({
+    container: 'regular-map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 4 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h5><b>${campground.title}</b></h5><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
